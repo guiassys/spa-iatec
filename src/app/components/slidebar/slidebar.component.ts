cc8d73f1ecb6cb43d968ac43fc30227cea3ad356
guiassys <guiassys@gmail.com>
2023-07-28 18:12:58 -0300

    Implementa menu dinâmico utilizando arrays.

--- src/app/components/slidebar/slidebar.component.ts
+++ src/app/components/slidebar/slidebar.component.ts
@@ -7,4 +7,37 @@ import { Component } from '@angular/core';
 })
 export class SlidebarComponent {
 
+  listMenu = [
+    {
+      label: 'Entrar',
+      fontIcon: 'login',
+      routerLink: 'login',
+    },
+    {
+      label: 'Sair',
+      fontIcon: 'logout',
+      routerLink: 'logout',
+    },
+    {
+      label: 'Home',
+      fontIcon: 'home',
+      routerLink: 'home',
+    },
+    {
+      label: 'Sobre',
+      fontIcon: 'info',
+      routerLink: 'about',
+    },
+    {
+      label: 'Contato',
+      fontIcon: 'contact_page',
+      routerLink: 'contact',
+    },
+    {
+      label: 'Serviços',
+      fontIcon: 'miscellaneous_services',
+      routerLink: 'service',
+    },
+  ];
+
 }
